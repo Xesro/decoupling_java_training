89378a3eb6ca18accdf9fe65c653f77b1e4d0ac5
Arthur Plebs <arthur.plebs@outlook.fr>
2022-03-10 15:20:09 +0100

    compose logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,9 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new ContextualLogger(new FileLogger("./logs"), name);
+        return new CompositeLogger(
+            new ContextualLogger(new FileLogger("./logs"), name),
+            new ContextualLogger(new ConsoleLogger(), name)
+        );
     }
 }
